2d6aae22dc693154ae71da1f167d6eecd9c460db
Vladimir Riz <resin.fhp@gmail.com>
2021-04-08 15:02:34 +0300

    feat: calc ingredients sum

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -2,11 +2,18 @@ import classes from './style.css';
 import BurgerIngredient from './BurgerIngredient';
 
 const Burger = (props) => {
-  const ingredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let ingredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((acc, next) => {
+      return acc.concat(next);
+    }, []);
+  if (ingredients.length === 0) {
+    ingredients = <p>Please choose ingredients</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
