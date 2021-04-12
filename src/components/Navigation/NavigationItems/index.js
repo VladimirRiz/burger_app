c5f4e6e22a4c4e3f4f6890e5a09956f21eb720b8
Vladimir Riz <resin.fhp@gmail.com>
2021-04-12 19:01:37 +0300

    feat: orders page

--- src/components/Navigation/NavigationItems/index.js
+++ src/components/Navigation/NavigationItems/index.js
@@ -3,10 +3,8 @@ import NavigationItem from './NavigationItem';
 
 const NavigationItems = (props) => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>
-      Burger
-    </NavigationItem>
-    <NavigationItem link="/">Checkout</NavigationItem>
+    <NavigationItem link="/">Burger</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
---
+++ src/components/Order/index.js
@@ -0,0 +1,10 @@
+import classes from './style.css';
+
+const Order = (props) => (
+  <div className={classes.Order}>
+    <p>Ingredients: Salad (1)</p>
+    <p>Total Price: USD 5.90</p>
+  </div>
+);
+
+export default Order;
