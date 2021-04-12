20108165d60c6bb7f33218bee5a2bebe9eb80177
Vladimir Riz <resin.fhp@gmail.com>
2021-04-12 19:08:13 +0300

    feat: navigation route

--- src/components/Navigation/NavigationItems/NavigationItem/index.js
+++ src/components/Navigation/NavigationItems/NavigationItem/index.js
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 const NavigationItem = (props) => (
   <li className={classes.NavigationItem}>
-    <NavLink to={props.link}>{props.children}</NavLink>
+    <NavLink to={props.link} activeClassName={classes.active} exact>
+      {props.children}
+    </NavLink>
   </li>
 );
 
